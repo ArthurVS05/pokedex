454cea0d5e5166af55b67deeae0ed893887ca4a4
ArthurVS05 <arthur.vincentsil@gmail.com>
2023-11-27 21:08:13 +0100

    creating branch components.2

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,9 +1,37 @@
 function PokemonCard() {
+    const pokemon = pokemonList[0];
+
     return (
         <figure>
-            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="" />
-            <figcaption>Bulbasaur</figcaption>
-        </figure>);
+            <figcaption>{pokemon.name}</figcaption>
+            {pokemon.imgSrc ? ( <img src={pokemon.imgSrc} alt={pokemon.name} />)
+            : (<p>???</p>)}
+
+        </figure>
+    );
 }
 
-export default PokemonCard
+const pokemonList = [
+    {
+        name: "bulbasaur",
+        imgSrc:
+            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+        name: "mew",
+    },
+];
+
+export default PokemonCard;
+
+
+
+// dans la fonction "PokemonCard", créer variable "pokemon".
+// "pokemon" contient le 1er Pokémon du tableau "pokemonList"
+// Utiliser pokemon.name et pokemon.imgSrc pour remplacer texte & attributs fixes de l'affichage
+// Afficher l'image : 
+// si pokemon.imgSrc est défini, afficher image avec la balise <img>
+// sinon, afficher <p> ???
+// utiliser ?:
+// Changement de Pokémon : 
+
